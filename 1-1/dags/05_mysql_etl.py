50b191828f4dfbb1e29157b67fac3feac212f9ad
exi <exisign777@gmail.com>
2026-04-09 14:42:25 +0900

    Add, 05_mysql_etl Practice

---
+++ 1-1/dags/05_mysql_etl.py
@@ -0,0 +1,98 @@
+'''
+- etl 간단하게 적용, 스마트 팩토리상 온도 센서에 대한 ETL 처리, mysql 사용
+'''
+# 1. 모듈 가져오기
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+# 추가분
+# from airflow.providers.mysql.operators.mysql import MysqlOperator
+# Load 처리 시, sql에 전처리 된 데이터를 밀어넣을 때 사용
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+
+#데이터
+import json
+import random
+import pandas as pd # 소량의 데이터(데이터 규모에 따라, 대량이면 spark)
+import os
+import logging
+
+# 2. 환경 변수
+# 프로젝트 내부 폴더에 ~/dags/data 지정
+# task 진행 간, 생성되는 파일을 동기화 하도록 위치 지정. -> 향후 s3(데이터 레이크)로 대체될 수 있음.
+#도커 내부에 생성된 컨테이너 상 워커내의 airflow 위치
+DATA_PATH = '/opt/airflow/dags/data' 
+os.makedirs(DATA_PATH, exist_ok=True)
+
+# 3. DAG 정의
+def _extract(**kwargs):
+    # 스마트 팩토리에 설치  오븐에 온도 센서에서, 데이터가 발생되면
+    # 데이터 레이크(s3, 어딘가에 존재) 에 쌓이고 있다. => 추출해서 가지고 오는 단계 
+    logging.info('task process : 온도 데이터 측정')
+    pass
+
+def _transform(**kwargs):
+    # _extract에서 추출한 데이터를 XCom을 통해서 획득
+    # 이 데이터를 df(pandas 사용, 소량데이터)로 로드 
+    # -> 섭씨를 화씨로 일괄처리(1번에 n개의 센서에서 데이터가 전달)
+    # 전처리 된 내용은 csv로 덤프 (s3로 업로드 고려)
+    logging.info('task process : 수집 데이터 변환')
+    pass
+    
+def _load(**kwargs):
+    # csv => df => mysql 적재
+    logging.info('task process : 변환 데이터 적재 처리')
+    pass
+
+# 2. DAG 정의
+with DAG (
+    dag_id = "05_mysql_etl", # 최소로 구성 된 필수 옵션.
+    description = "etl 수행하여, mysql에 온도 센서 데이터 적재",
+    default_args = {
+        'owner'             : 'de_2team_manager',
+        'retries'           : 1,                 
+        'retry_delay'      : timedelta(minutes=1)
+    },    
+    schedule_interval = '@daily',   
+    start_date = datetime(2026,2,25),
+    catchup = False,                 
+    tags = ['mysql', 'etl']
+) as dag:
+    # 4. task 정의
+    # task_create_table = MysqlOperator(
+    #     # 테이블 생성, if not exists를 사용하여 무조건 sql이 일단 수행되게 구성
+    #     # -> 아니라면, fail 발생함(2회차 부터)
+    #     # 최초는 생성, 존재하면 pass => if not exists
+    #     task_id = "create_table",
+    #     # 연결정보
+    #     mysql_conn_id = "mysql_connection",  # 대시보드(admin > connections > 하위에 사전에 등록
+    #     # sql
+    #     sql = '''
+    #         CREATE TABLE IF NOT EXISTS sensor_readings (
+    #             id INT AUTO_INCREMENT PRIMARY KEY,
+    #             sensor_id VARCHAR(50),
+    #             timestamp DATETIME,
+    #             temperature_c FLOAT,
+    #             temperature_f FLOAT,
+    #             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    #         );
+    #     '''
+    # )
+    task_extract    = PythonOperator(
+        task_id = "extract",
+        python_callable= _extract
+    )
+    task_transform       = PythonOperator(
+        task_id = "transform",
+        python_callable = _transform
+    )
+    task_load        = PythonOperator(
+        task_id = "load",
+        python_callable = _load
+    )
+
+    # 4. 의존성 정의 -> 시나리오별 준비
+    # task_create_table >> task_extract >> task_transform >> task_load
+    task_extract >> task_transform >> task_load
+    pass
