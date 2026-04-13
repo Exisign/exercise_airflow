b8ff8aefe159fb12cfdfad7412ae96792a2da4e6
exi <exisign777@gmail.com>
2026-04-13 10:33:08 +0900

    Add, Code logic for mysql insertion

--- 1-1/dags/05_mysql_etl.py
+++ 1-1/dags/05_mysql_etl.py
@@ -99,33 +99,50 @@ def _load(**kwargs):
     # 1. csv 경로 획득
     ti = kwargs['ti']
     csv_file_path = ti.xcom_pull(task_ids = 'transform')
-    df = pd.read_csv(csv_file_path)
-    
+
+    # 2. csv -> df (도입 근거 => 소규모 데이터이기 때문에, Pandas로 충분)
+    # (대규모일 경우, spark --> 보통은 cloud로 넘어간다)
+    df = pd.read_csv(csv_file_path)    
     logging.info(f'df.align : {df.values.tolist()}')
 
-    # 2. csv -> df
+
 
     # 3. mysql 연결 => MySqlHook 사용
-    mysql_hook  = MySqlHook(mysql_conn_id = 'mysql_default')
-    conn        = mysql_hook.get_conn() #커넥션 획득
+    mysql_hook  = MySqlHook(mysql_conn_id = 'mysql_connection')
+    conn        = mysql_hook.get_conn()
+    #커넥션 획득, 해당 부분도 I/O의 영향을 받기 때문에, 예외처리 / with 문 사용
 
     # 4. 전체를 try ~ except로 감싸기 (I/O)
+    # 실제는 실패 작업인데, 성공으로 오인할 수 있다. -> 예외 던지기 필요함.
     try:
     # 4. 커서를 획득하여 insert 구문 사용ㅇ
         with conn.cursor() as cursor:    
             # 4-1 insert 구문 사용
-            # sql = "insert into "
-            # params = []
-            # cusor.executemany( sql, params )
+            sql = '''
+            insert into sensor_readings 
+            (sensor_id, timestamp, temperature_c, temperature_f)
+            values (%s, %s, %s, %s)
+            '''
+            # 여러 데이터를 한번에 넣을 때 유용 => executemany() 대응
+            params = [
+                ( data['sensor_id'], data['timestamp'],
+                  data['temperature_c'], data['temperature_f'])
+                for _, data in df.iterrows() # 데이터가 없을 때까지 반복함. -> 데이터가 한세트씩 추출 됨
+            ]
+            logging.info(f'입력할 데이터(파라미터) {params}')
+            cursor.executemany( sql, params )
             # 4-2. 커밋
-        
-         pass
+            conn.commit()
+            logging.info(f'mysql에 적재 완료')
+            pass
     except Exception as e:
+        logging.info(f'적재 오류 : {e}')
         pass
     finally :
         # 연결 종료
         if conn:
             conn.close()
+            logging.info(f'mysql 연결 종료')
     pass
 
 # 2. DAG 정의
