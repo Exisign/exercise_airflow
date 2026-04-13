f9463e8546d043ad6f047f0c8e1104e71ede2905
exi <exisign777@gmail.com>
2026-04-13 13:05:29 +0900

    Add, separated transform Code

--- 1-1/dags/06_multi_dag_2step_transform.py
+++ 1-1/dags/06_multi_dag_2step_transform.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 from datetime import datetime, timedelta
 
@@ -9,17 +10,13 @@ import logging
 DATA_PATH = '/opt/airflow/dags/data' 
 
 def _transform(**kwargs):
-    # _extract에서 추출한 데이터를 XCom을 통해서 획득
-    # 이 데이터를 df(pandas 사용, 소량데이터)로 로드 
-    # -> 섭씨를 화씨로 일괄처리(1번에 n개의 센서에서 데이터가 전달)
-    # 전처리 된 내용은 csv로 덤프 (s3로 업로드 고려)
-
+    # _extract에서 추출한 데이터를 다른 Dag에서 전달한 conf를 활용하여 추출 -> "dag_run"활용
     # 1. XCOM을 통해서 이전 task에서 전달한 데이터 획득
-    ti = kwargs['ti']
+    dag_run = kwargs['dag_run']
 
     # 2. task 본연의 업무  => XCOM 활용
     # 특정 task가 기록한 데이터를 획득
-    json_file_path = ti.xcom_pull(task_ids = 'extract')
+    json_file_path = dag_run.conf.get('json_path')
     logging.info(f'전달 받은 데이터 {json_file_path }')
     
     # 이 데이터를 df(pandas 사용 , 소량 데이터)로 로드
@@ -64,7 +61,22 @@ with DAG (
         python_callable = _transform
     )
 
-    # 4. 의존성 정의 -> 시나리오별 준비
-    task_transform
-    # task_extract >> task_transform >> task_load
+    task_trigger_load_dag_run = TriggerDagRunOperator(
+        task_id = 'trigger_load',
+        # 트리거 대상
+        trigger_dag_id='06_multi_dag_3step_load',
+        # 전달할 데이터 -> xcom을 통해서 획등가능 (동일 DAG에 존재하기 때문 -> jinja 템플릿 활용)
+        conf = {
+            # 필요시, 기타 정보도 전달 가능함.
+            "csv_path" : "{{ task_instance.xcom_pull(task_ids='transform') }}"
+        },
+        # dag 수행 시간 세팅 => 동일하게 맞추겠다. PythonOperator의 작동시간과 (컨셉)
+        # 1개의 DAG에서 task 간 시간차와 유사하게, 혹은 거의 동일하게 맞추고자 하는 컨셉임.
+        reset_dag_run = True, #직전 단계에서 거의 바로 연결되는 시간 설정
+        # 기타 설정
+        # 타 Dag가 수행하라는 명령을 전달하면, 대기 없이 바로 본 종료(비동기 처리)
+        wait_for_completion = False 
+    )
+
+    task_transform >> task_trigger_load_dag_run
     pass
